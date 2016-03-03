8d8f05aa2c09bac39bc5f40016b560a54f33dd6f
YOmna <yomna.m.hafez@gmail.com>
2016-03-03 22:21:32 +0200

    map view with current user location pinned is done

--- app/src/main/java/mobiledoctors/yomna/mobiledoctorstask/Home.java
+++ app/src/main/java/mobiledoctors/yomna/mobiledoctorstask/Home.java
@@ -2,23 +2,98 @@ package mobiledoctors.yomna.mobiledoctorstask;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.picasso.Picasso;
 
 
 /**
  * Created by yomna on 3/3/16.
  */
-public class Home extends Activity {
+public class Home extends FragmentActivity {
+    private DrawerLayout mDrawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        TextView userInfo = (TextView)findViewById(R.id.userInfo);
-        //profilePictureURL
-        userInfo.setText(PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("profilePictureURL", "Oops, profilePictureURL not found ! "));
-      //  userInfo.setText(PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("userName", "Oops, User name not found ! "));
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        initializeMap();
+        initializeDrawer();
+
+    }
+
+
+    private void initializeMap() {
+
+        final GoogleMap map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        map.setMyLocationEnabled(true);
+        GoogleMap.OnMyLocationChangeListener myLocationChangeListener = new GoogleMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+                LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16.0f));
+            }
+        };
+        map.setOnMyLocationChangeListener(myLocationChangeListener);
+    };
+
+
+    private void initializeDrawer() {
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nvView);
+        View headerLayout = navigationView.inflateHeaderView(R.layout.nav_header);
+
+        TextView userName = (TextView) headerLayout.findViewById(R.id.tvUserName);
+        userName.setText(PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("userName", "Oops, User name not found ! "));
+
+        ImageView profilePicture = (ImageView) headerLayout.findViewById(R.id.ivProfilePicture);
+        Picasso.with(this)
+                .load(PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("profilePictureURL", ""))
+                .placeholder(R.drawable.test1)
+                .error(R.drawable.test1)
+                .into(profilePicture);
+    };
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    // `onPostCreate` called when activity start-up is complete after `onStart()`
+    // NOTE! Make sure to override the method with only a single `Bundle` argument
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
     }
 }
