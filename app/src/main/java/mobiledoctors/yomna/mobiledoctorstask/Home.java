7cd555b48cc76e3dca2f7f81c9fe6088010aa446
YOmna <yomna.m.hafez@gmail.com>
2016-03-05 21:47:27 +0200

    get distance using distanceTo is done

--- app/src/main/java/mobiledoctors/yomna/mobiledoctorstask/Home.java
+++ app/src/main/java/mobiledoctors/yomna/mobiledoctorstask/Home.java
@@ -184,6 +184,17 @@ public class Home extends FragmentActivity {
 //            }
 //        }
 
+        Location locationA = new Location("point A");
+
+        locationA.setLatitude(lat1);
+        locationA.setLongitude(lon1);
+
+        Location locationB = new Location("point B");
+
+        locationB.setLatitude(lat2);
+        locationB.setLongitude(lon2);
+
+        float distance = locationA.distanceTo(locationB);
     }
 
     private double deg2rad(double deg) {
